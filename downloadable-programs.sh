3864f3318cc1304c53f5a14ed4da31c6ca78944f
Andreas Scherer <andreas_github@freenet.de>
2016-09-25 10:36:32 +0200

    Two new example programs.

--- downloadable-programs.sh
+++ downloadable-programs.sh
@@ -33,7 +33,7 @@ for FILE in sham.w obdd.w.gz reflect.w.gz reflect.ch.gz hull.w hulls.w hullt.w \
 	back-mxn-words-new.w back-mxn-words-mxn.w back-pdi.w \
 	commafree-eastman.w commafree-eastman-new.w sat-commafree.w \
 	back-commafree4.w back-skeleton-shortest.w back-dissect.w \
-	ulam-gibbs.w ulam-gibbs.1
+	ulam-gibbs.w ulam-gibbs.1 dlx1.w dlx2.w
 do
 	echo "cURLing $FILE"
 
