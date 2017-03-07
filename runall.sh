8ccef4d970438675cddf01024fa0094258e26687
Andreas Scherer <andreas_github@freenet.de>
2017-03-07 17:12:28 +0100

    Update commit format.

--- runall.sh
+++ runall.sh
@@ -7,22 +7,22 @@ while getopts p:v: opt
 do
 	case $opt in
 		p) path=$OPTARG ;;
-		v) version="-$OPTARG" ;;
+		v) version=$OPTARG ;;
 		?) exit 1 ;;
 	esac
 done
 
-git checkout -b runall-$path$version
+git checkout -b "runall-$path-V$version"
 
 for i in *.w
 do
 	$path/ctangle $i
 	git add `basename $i .w`.c
-	git commit -m "ctangle$version $i."
+	git commit -m "ctangle [$version] $i."
 
 	$path/cweave $i
 	git add `basename $i .w`.idx `basename $i .w`.scn `basename $i .w`.tex
-	git commit -m "cweave$version $i."
+	git commit -m "cweave [$version] $i."
 done
 
 git checkout master
