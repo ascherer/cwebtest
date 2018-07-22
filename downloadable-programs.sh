1c5f1775705b40774c8921d4136ac1f52f86e4b5
Andreas Scherer <andreas_github@freenet.de>
2018-07-22 11:54:25 +0200

    Extended download options.

--- downloadable-programs.sh
+++ downloadable-programs.sh
@@ -1,29 +1,49 @@
 #!/bin/sh
-# Invoke with option '--get' to actually download files
-GET=${1:--no-get}
-STANFORD=https://www-cs-faculty.stanford.edu/~knuth/programs
+# Invoke with option '-d|--get' to actually download files
+# Invoke with option '-f <FILE>|--file <FILE>' to handle singular file(s)
+# Invoke with option '-r <URL>|--remote <URL>' to change server address
+
+if OPTS=$(getopt -n downloadable-programs.sh -o df:r: \
+	--long file:,get,remote: -- "$@")
+then eval set -- "$OPTS"
+else echo "Failed to parse options." >&2; exit 1; fi
+
+FILES=`cat downloadable-programs.lst`
+GET=false
+REMOTE='https://www-cs-faculty.stanford.edu/~knuth/programs'
+
+while true
+do
+	case "$1" in
+		-d | --get ) GET=true; shift ;;
+		-f | --file ) FILES="$2"; shift 2 ;;
+		-r | --remote ) REMOTE="$2"; shift 2 ;;
+		-- ) shift; break ;;
+		* ) break ;;
+	esac
+done
 
 # Generic download function
 get_file () # path/to/file
 {
 	FILE=`basename $1`
-	REMOTE=`dirname $1`
+	DIR=`dirname $1`
 
-	echo "cURLing $REMOTE/$FILE"
+	echo "cURLing $DIR/$FILE"
 
-	if [ "$GET" = "--get" ]
+	if $GET
 	then
 		if [ -f $FILE ]
-		then curl -k -R $REMOTE/$FILE -z $FILE -o $FILE
-		else curl -k -R $REMOTE/$FILE -o $FILE
+		then curl -k -R $DIR/$FILE -z $FILE -o $FILE
+		else curl -k -R $DIR/$FILE -o $FILE
 		fi
 	else
-		curl -k -I $REMOTE/$FILE
+		curl -k -I $DIR/$FILE
 	fi
 }
 
 # Process extended list of files in 'programs/' directory
-for FILE in `cat downloadable-programs.lst`
+for FILE in $FILES
 do
-	get_file $STANFORD/$FILE
+	get_file "$REMOTE/$FILE"
 done
