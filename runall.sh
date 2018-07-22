75b870d243c7193b02be931564d5750944d8e256
Andreas Scherer <andreas_github@freenet.de>
2018-07-22 11:58:44 +0200

    Use boolean logic instead of numerics.

--- runall.sh
+++ runall.sh
@@ -2,12 +2,12 @@
 
 path= # Path to CWEB, e.g., /usr/bin
 version= # Version of CWEB, e.g., 3.64b-22p
-changes=0 # Apply associated change files (if any)
+changes=false # Apply associated change files (if any)
 
 while getopts chp:v: opt
 do
 	case $opt in
-		c) changes=1 ;;
+		c) changes=true ;;
 		p) path=$OPTARG ;;
 		v) version=$OPTARG ;;
 		?) exit 1 ;;
@@ -23,7 +23,7 @@ fi
 
 # Take changes into account
 testbranch="runall-$path-V$version"
-[ $changes -ne 0 ] && testbranch="$testbranch-changes"
+$changes && testbranch="$testbranch-changes"
 git checkout -b $testbranch || exit 1 # Avoid 'master' desaster
 
 # Process all CWEB main sources
@@ -47,7 +47,7 @@ do
 	>&2 git commit -m "cweave [$version] $i."
 
 	# Process all associated CWEB change files ...
-	[ $changes -ne 0 ] || continue
+	$changes || continue
 	for c in $bi*.ch
 	do
 		[ -f $c ] || continue # ... if any
@@ -66,7 +66,7 @@ do
 done
 
 # Apply 'special' changefiles that do not quite fit into the standard scheme
-if [ $changes -ne 0 ]
+if $changes
 then
 	for i in s t tr
 	do
